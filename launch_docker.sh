c3d290cc8f84b8588d24b6a334cbf88ac1bae86f
Erik Vroon <erik.vroon22@gmail.com>
2021-03-03 12:51:56 +0100

    Improve launch_docker.sh

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 
 COMMAND="/bin/bash"
+volumes="-v $(pwd):/undistort"
+interactive_options="-it"
+
+common_prefix() {
+  local n=0
+  while [[ "${1:n:1}" == "${2:n:1}" ]]; do
+    ((n++))
+  done
+  echo "${1:0:n}"
+}
 
 while test $# -gt 0
 do
     case "$1" in
         --run)
+            interactive_options=""
 			input_model="$2"
 			input_img="$3"
 			output_img="$4"
             COMMAND="build/undistort $input_model $input_img $output_img"
+
+            # Determine overlapping part of input paths so that we can bind that directory as volume.
+            volume=`common_prefix $input_img $output_img`
+
+            echo "Adding Docker volume:" $volume
+            volumes="$volumes -v $volume:$volume"
+
             shift
             shift
             shift
         ;;
         --run-example)
+            interactive_options=""
             COMMAND="./build.sh && build/undistort examples/calib_results_fisheye.txt examples/test_fisheye.jpg examples/undistorted_perspective.jpg"
         ;;
         --build)
+            interactive_options=""
             COMMAND="./build.sh"
         ;;
     esac
     shift
 done
 
-docker run --rm \
-    -v $(pwd):/undistort \
+docker run $interactive_options --rm \
+    $volumes \
     $USER/undistort-functions:latest \
     bash -c "$COMMAND"
